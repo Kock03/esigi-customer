3c8c8cb4265a17de4cfb58de232548fbb5bd2def
davikrig-ha <daviluia2@gmail.com>
2022-05-23 08:07:51 -0300

    route

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/cliente/lista',
+    redirectTo: 'cliente/lista',
     pathMatch: 'full',
   },
 
