8ff74a47294d12e4583c053723699668becf25f9
biancanilsen <biancanilsen.entra21@gmail.com>
2022-05-23 16:45:29 -0300

    confirm dialog

--- webapp/src/app/modules/customer/customer-list/customer-list.component.ts
+++ webapp/src/app/modules/customer/customer-list/customer-list.component.ts
@@ -7,6 +7,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 
 import { CustomerProvider } from 'src/providers/customer.provider';
+import { ConfirmDialogService } from 'src/services/confirn-dialog.service';
 import { SnackBarService } from 'src/services/snackbar.service';
 
 export interface Customer {
@@ -38,7 +39,8 @@ export class CustomerListComponent implements OnInit {
     private liveAnnouncer: LiveAnnouncer,
     private router: Router,
     private customerProvider: CustomerProvider,
-    private snackbarService: SnackBarService
+    private snackbarService: SnackBarService,
+    private dialogService: ConfirmDialogService,
   ) { }
 
   ngOnInit(): void {
@@ -75,6 +77,17 @@ export class CustomerListComponent implements OnInit {
   }
 
   async deleteCustomer(customerId: any) {
+    const options = {
+      data: {
+        title: 'Atenção',
+        subtitle: 'Você tem certeza que deseja excluir este cliente?',
+      },
+      panelClass: 'confirm-modal',
+    };
+
+    this.dialogService.open(options);
+    this.dialogService.confirmed().subscribe(async confirmed => {
+      if (confirmed) {
         try {
           const customers = await this.customerProvider.destroy(customerId);
           this.getCustomerList();
@@ -83,6 +96,8 @@ export class CustomerListComponent implements OnInit {
           console.log('ERROR 132' + error);
         }
       }
-  }
+    }
+  )}
+}
 
 
